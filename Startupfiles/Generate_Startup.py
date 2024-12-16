4dce90052bb7538e667d9ba7af1473bb8e4f01aa
IPGAUTOMOTIVEINDIA <dhrithi.dk@ipg-automotive.com>
2024-12-16 15:33:10 +0530

    Add files via upload

---
+++ Startupfiles/Generate_Startup.py
@@ -0,0 +1,34 @@
+filem= "Startup_Example_Template"                     # Original file
+lookup = 'nv.Speed = '
+lookup_1 = 'nv.Lateral_Offset = '         
+lookup_5 = 'kv.SimParameters\:DStore.OutPath = SimOutput/LaneChangeISO_'  
+
+file1 = open(filem)
+all_lines = file1.readlines()
+l_num = open(filem).read()[:open(filem).read().index(lookup)].count('\n')
+l_num1 = open(filem).read()[:open(filem).read().index(lookup_1)].count('\n')
+l_num5 = open(filem).read()[:open(filem).read().index(lookup_5)].count('\n')
+
+arr = all_lines[l_num].split()
+arr1 = all_lines[l_num1].split()
+arr_5 = all_lines[l_num5].split()
+
+def listToString(s):
+    # initialize an empty string
+    str1 = " "
+    # return string
+    return (str1.join(s))
+
+
+a_list = list(range(60, 135, 5))   # range of values to be changed each time
+b_list = list(range(2, 5, 1))
+for x in a_list:  # for every value in the range
+    for y in b_list:
+        with open(filem, 'r', encoding='utf-8') as file:
+            data = file.readlines()
+        data[l_num] = lookup + str(x) + '\n'
+        data[l_num1] = lookup_1 + str(y) + '\n'
+        data[l_num5] = lookup_5 + 'speed_' + str(x) + 'LatOff_' + str(y) + '\n'
+        
+        with open(filem + 'S_'+str(x) +'_O_'+ str(y), 'w', encoding='utf-8') as file:
+            file.writelines(data)
