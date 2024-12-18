003949e491a7c0307b04ba328ac79f95cb5887a7
IPGAUTOMOTIVEINDIA <dhrithi.dk@ipg-automotive.com>
2024-12-18 10:01:16 +0530

    Update the path

--- Data/TestRun/Run_headless.sh
+++ Data/TestRun/Run_headless.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /var/lib/jenkins/workspace/EXECUTABLE\ BUILD/CM_Project/
+#cd /var/lib/jenkins/workspace/EXECUTABLE\ BUILD/CM_Project/
 for file in /var/lib/jenkins/workspace/EXECUTABLE\ BUILD/CM_Project/Startupfiles/*
 do
   /var/lib/jenkins/workspace/EXECUTABLE\ BUILD/CM_Project/src/CarMaker.linux64 "$file" -v -screen -dstore
