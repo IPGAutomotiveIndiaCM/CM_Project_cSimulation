bc71826b81adcb1673b7caf0314f52657ecd885d
IPGAutomotiveIndiaCM <dhrithi.dk@ipg-automotive.com>
2024-12-20 10:00:31 +0530

    SimOutput results update: 2024-12-20_10-00-30

--- src/app_tmp.c
+++ src/app_tmp.c
@@ -56,10 +56,10 @@ static const char *CompileFlags[] = {
 
 tAppStartInfo   AppStartInfo = {
     "Car_Generic <insert.your.version.no>",          /* App_Version         */
-    "67",          /* App_BuildVersion    */
+    "68",          /* App_BuildVersion    */
     "jenkins",     /* App_CompileUser     */
     "dhrithiipg",         /* App_CompileSystem   */
-    "2024-12-20 09:57:49",  /* App_CompileTime */
+    "2024-12-20 09:59:20",  /* App_CompileTime */
 
     CompileFlags,                /* App_CompileFlags  */
     CompileLibs,                 /* App_Libs          */
