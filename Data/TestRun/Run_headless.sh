be46d61198811c19f130970919a8fa02e909dbe4
IPGAUTOMOTIVEINDIA <dhrithi.dk@ipg-automotive.com>
2025-01-10 15:00:08 +0530

    Update Run_headless.sh

--- Data/TestRun/Run_headless.sh
+++ Data/TestRun/Run_headless.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-for file in /home/dhrithi/dk1/startupfile/Startup_*; do
+
+# Loop through files starting with 'Startup_' in the specified directory
+for file in /var/lib/jenkins/workspace/cSimulation/Startupfiles/Startup_*; do
   if [[ -f "$file" ]]; then  # Check if it's a regular file
-    /home/dhrithi/dk1/src/CarMaker.linux64 "$file" -v -screen -dstore
+    echo "Processing file: $file"  # Optional: Print the file being processed
+    /var/lib/jenkins/workspace/cSimulation/src/CarMaker.linux64 "$file" -v -screen -dstore
   fi
 done
