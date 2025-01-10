173d4ddab9615b063a2638c361779cf27a2551c3
IPGAUTOMOTIVEINDIA <dhrithi.dk@ipg-automotive.com>
2025-01-10 15:13:29 +0530

    Update Run_headless.sh

--- Data/TestRun/Run_headless.sh
+++ Data/TestRun/Run_headless.sh
@@ -3,6 +3,7 @@
 # Loop through files in the directory
 for file in /var/lib/jenkins/workspace/cSimulation/Startupfiles/Startup_Example_*; do
   # Check if the file exists to prevent errors in case no files match the pattern
+  echo file;
   if [[ -f "$file" ]]; then
     # Run your command for each matching file
     //var/lib/jenkins/workspace/cSimulation/src/CarMaker.linux64 "$file" -v -screen -dstore
