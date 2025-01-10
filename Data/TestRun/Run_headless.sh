d22296434361da8d0db82b845fceb944a44ed880
IPGAUTOMOTIVEINDIA <dhrithi.dk@ipg-automotive.com>
2025-01-10 14:57:03 +0530

    Update Run_headless.sh

--- Data/TestRun/Run_headless.sh
+++ Data/TestRun/Run_headless.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for file in /home/dhrithi/dk1/startupfile/start_*; do
+for file in /home/dhrithi/dk1/startupfile/Startup_*; do
   if [[ -f "$file" ]]; then  # Check if it's a regular file
     /home/dhrithi/dk1/src/CarMaker.linux64 "$file" -v -screen -dstore
   fi
