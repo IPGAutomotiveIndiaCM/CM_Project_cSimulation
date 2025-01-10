955d74f3ffa483d08bc0374e55b534d20c3cdcc7
IPGAUTOMOTIVEINDIA <dhrithi.dk@ipg-automotive.com>
2025-01-10 15:06:07 +0530

    Update Run_headless.sh

--- Data/TestRun/Run_headless.sh
+++ Data/TestRun/Run_headless.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# Check if any files match the pattern before entering the loop
-shopt -s nullglob  # Enable nullglob to prevent glob pattern from being expanded if no files match
-
-# Loop through files starting with 'Startup_' in the specified directory
-for file in /var/lib/jenkins/workspace/cSimulation/Startupfiles/Startup_*; do
-  if [[ -f "$file" ]]; then  # Check if it's a regular file
-    echo "Processing file: $file"  # Optional: Print the file being processed
-    /var/lib/jenkins/workspace/cSimulation/src/CarMaker.linux64 "$file" -v -screen -dstore
+# Loop through files in the directory
+for file in /var/lib/jenkins/workspace/cSimulation/Startupfiles/Startup_Example_*; do
+  # Check if the file exists to prevent errors in case no files match the pattern
+  if [[ -f "$file" ]]; then
+    # Run your command for each matching file
+    //var/lib/jenkins/workspace/cSimulation/src/CarMaker.linux64 "$file" -v -screen -dstore
   fi
 done
-
-shopt -u nullglob  # Disable nullglob after the loop
