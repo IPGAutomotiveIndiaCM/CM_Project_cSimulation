5bf56bba91f58015b8294c123a559e77fcfa70d9
IPGAUTOMOTIVEINDIA <dhrithi.dk@ipg-automotive.com>
2025-01-21 10:49:41 +0530

    Update Run_headless.sh

--- Data/TestRun/Run_headless.sh
+++ Data/TestRun/Run_headless.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Loop through files in the directory
-for file in /var/lib/jenkins/workspace/cSimulation/Startupfiles/Startup_Example_*; do
+for file in /home/dockeration/Startupfiles/Startup_Example_*; do
   # Check if the file exists to prevent errors in case no files match the pattern
-  echo Hello World;
-  if [[ -f "$file" ]]; then
+  echo TEST RUN;
+  if [[ -f "$file" ]]; then 
     # Run your command for each matching file
-    //var/lib/jenkins/workspace/cSimulation/src/CarMaker.linux64 "$file" -v -screen -dstore
+    echo inside if loop
+    //home/dockeration/src/CarMaker.linux64 "$file" -v -screen -dstore
   fi
 done
