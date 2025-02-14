a7d988ddee268a703521c900c26ed497dc51e45d
IPGAUTOMOTIVEINDIA <dhrithi.dk@ipg-automotive.com>
2025-02-14 14:06:01 +0530

    Update Run_headless.sh

--- Data/Run_headless.sh
+++ Data/Run_headless.sh
@@ -1,1 +1,12 @@
-ff
+#!/bin/bash
+
+# Loop through files in the directory
+for file in /home/cSimulation/Startupfiles/Startup_Example_*; do
+  # Check if the file exists to prevent errors in case no files match the pattern
+  echo TEST RUN;
+  if [[ -f "$file" ]]; then 
+    # Run your command for each matching file
+    echo inside if loop
+    //home/cSimulation/src/CarMaker.linux64 "$file" -v -screen -dstore
+  fi
+done
