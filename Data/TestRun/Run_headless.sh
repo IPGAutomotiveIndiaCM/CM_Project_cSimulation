a4e85db2b9e9875cbb82ce5931517ef2c41532c5
IPGAUTOMOTIVEINDIA <dhrithi.dk@ipg-automotive.com>
2025-07-23 16:28:23 +0530

    Update Run_headless.sh

--- Data/TestRun/Run_headless.sh
+++ Data/TestRun/Run_headless.sh
@@ -3,7 +3,6 @@ for file in /home/cSimulation/Startupfiles/*
 do
   /home/cSimulation/src/CarMaker.linux64 "$file" -v -screen -dstore
 done
-chmod +x /home/export_movie.sh
-/home/export_movie.sh
+
 
 
